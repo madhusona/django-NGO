1a6677daa119d4a093db9149509911bfc3d44a8b
Ramesh <letters4ramesh@gmail.com>
2018-11-26 17:05:23 +0530

    maps

--- frugal/models.py
+++ frugal/models.py
@@ -3,17 +3,49 @@ from django.db import models
 from django.forms import ModelForm
 from django import forms
 from datetime import datetime
+from django.core.exceptions import ValidationError
+import re
+
+
 # Create your models here.
 class NGO(models.Model):
-     Organization_Name = models.CharField(max_length=200,help_text="Required. Maximum 200 Characters")
-     Contact_Person = models.CharField(max_length=50,help_text="Required. Maximum 50 Characters")
-     Email_id = models.CharField(max_length=50,help_text="Required. Maximum 50 Characters")
-     Mobile_no = models.CharField(max_length=10,help_text="Required. Maximum 10 Characters")
-     Address   =   models.CharField(max_length=200,help_text="Required. Door No and Street. Maximum 200 Characters")
-     City = models.CharField(max_length=50,help_text="Required. Maximum 50 Characters")
-     Pincode = models.CharField(max_length=6,help_text="Required. Maximum 6 Characters")
-     Website = models.CharField(max_length=30,null=True,help_text="Optional. Maximum 30 Characters")
-     Established_on = models.DateField(help_text="Required.")
+    Organization_Name = models.CharField(max_length=200,help_text="Maximum 200 Characters")
+    Contact_Person = models.CharField(max_length=50,help_text="Maximum 50 Characters")
+    Email_id = models.CharField(max_length=50,primary_key=True,help_text="Maximum 50 Characters")
+    Mobile_no = models.CharField(max_length=10,help_text="Should be 10 Characters")
+    Address   =   models.CharField(max_length=200,help_text="Door No and Street. Maximum 200 Characters")
+    City = models.CharField(max_length=50,help_text="Maximum 50 Characters")
+    Latitude = models.DecimalField(null=True,blank=True,max_digits=13, decimal_places=10)
+    Longitude = models.DecimalField(null=True,blank=True,max_digits=13, decimal_places=10)
+    Pincode = models.CharField(max_length=6,help_text="Should be 6 Characters")
+    Website = models.CharField(max_length=30,blank=True,null=True,help_text="Maximum 30 Characters")
+    Established_on = models.DateField(help_text="Required.")
+
+    def __str__(self):
+          return self.Email_id
+
+
+    
+class NGO_Profile(models.Model):
+    NGO = models.OneToOneField(NGO,on_delete=models.CASCADE)
+    Overview = models.CharField(max_length=10000,help_text="About your Organization")
+    Cover_Photo = models.ImageField(upload_to = 'pictures',null=True, blank=True)
+    Account_STATUS = (
+        ('R', 'Registered'),
+        ('V', 'Verified'),
+        ('A', 'Activated'),
+        ('D', 'Deactivated'),
+    )
+
+    Status = models.CharField(max_length=1,choices=Account_STATUS,default='R')
+
+class NGO_Registration(models.Model):
+    NGO = models.ForeignKey(NGO,on_delete=models.CASCADE)
+    Recognized_Body = models.CharField(max_length=100)
+    Registration_Number = models.CharField(max_length=100)
+
+
+
      
 class SignupForm(ModelForm):
     class Meta:
@@ -22,13 +54,40 @@ class SignupForm(ModelForm):
         widgets = {
             'Address': forms.Textarea, 'Email_id':forms.EmailInput,'Established_on':forms.SelectDateWidget(years=range(1900,datetime.today().year+1))
         }
-    def clean(self):
-        data = self.cleaned_data
-        if len(data['Mobile_no']) != 10:
-            raise forms.ValidationError('Mobile Number Should be 10 Digits!!!')
-        if len(data['Pincode']) != 6:
-            raise forms.ValidationError('Pincode Should be 6 Digits!!!')
-        return self.cleaned_data
+    def clean_Mobile_no(self):
+        data = self.cleaned_data['Mobile_no']
+        
+        if len(data) != 10 or not data.isdigit():
+            raise ValidationError('Invalid Mobile No')
+        return data
+    def clean_Pincode(self):
+        data = self.cleaned_data['Pincode']
+        
+        if len(data) != 6 or not data.isdigit():
+            raise ValidationError('Invalid Pincode')
+        return data
+
+    def clean_Email_id(self):
+        data = self.cleaned_data['Email_id']
+        if NGO.objects.filter(Email_id=self.cleaned_data['Email_id']).exists():
+            raise ValidationError('Email ID already Exsists')
+        return data
+
+class ProfileForm(forms.Form):
+    Overview = forms.CharField(widget=forms.Textarea)
+    Cover_Photo = forms.ImageField()
+
+class NGO_locationForm(forms.Form):
+    latitude = forms.DecimalField()
+    longitude = forms.DecimalField()
+
+    
+
+
+
+   
+
+
 
 
 
--- frugal/views.py
+++ frugal/views.py
@@ -1,23 +1,79 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from . import models
+from .models import NGO_Profile
+from .models import NGO
+from .models import NGO_Registration
+
 
 
 # Create your views here.
 def index(request):
     return render(request,'frugal/index.html')
 
+
 def register(request):
     args = {}
     if request.method == 'POST':
         form = models.SignupForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('index')
+            #form.save()
+            request.session['step1_form']=request.POST
+            #request.session['ngo'] = form.cleaned_data['Email_id']
+            return redirect('Map')
     else:
         form = models.SignupForm()
     args['form'] = form
-    return render(request, 'frugal/Register.html', {'form': form}) 
+    return render(request, 'frugal/Register.html', {'form': form})
+
+def Map(request):
+    if request.method == 'POST':
+        form = models.NGO_locationForm(request.POST, request.FILES)
+        if form.is_valid():
+            request.session['latitude']=float(form.cleaned_data['latitude'])
+            request.session['longitude']=float(form.cleaned_data['longitude'])
+            return redirect('Profile')
+    else:
+        form=models.NGO_locationForm()
+    
+    return render(request,'frugal/map.html',{'from':form})
+
+def Profile(request):
+    if request.method == 'POST':
+        form = models.ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            reg_form= models.SignupForm(request.session['step1_form'])
+            reg_form.save()
+           
+            Record = NGO.objects.get(Email_id=reg_form.cleaned_data['Email_id'])
+            Record.Latitude = float(request.session['latitude'])
+            Record.Longitude = float(request.session['longitude'])
+            Record.save()
+            profile = NGO_Profile()
+            #profile.NGO = NGO.objects.get(Email_id=request.session['ngo'])
+            profile.NGO=NGO.objects.get(Email_id=reg_form.cleaned_data['Email_id'])
+            profile.Overview = form.cleaned_data['Overview']
+            profile.Cover_Photo = form.cleaned_data['Cover_Photo']
+            profile.Status ='R'
+            profile.save()
+
+                
+            for count in range(1,4):
+                field1="Recognized_Body"+str(count)
+                if request.POST.get(field1)!=None:
+                    registration1=NGO_Registration()
+                    registration1.NGO=NGO.objects.get(Email_id=reg_form.cleaned_data['Email_id'])
+                    registration1.Recognized_Body=request.POST.get("Recognized_Body"+str(count))
+                    registration1.Registration_Number=request.POST.get("Registration_Number"+str(count))
+                    registration1.save()
+                 
+            del request.session['step1_form']
+            return redirect('index')
+    else:
+        form=models.ProfileForm()        
+    return render(request, 'frugal/Profile.html',{'form': form})
+
+
    
 
 
