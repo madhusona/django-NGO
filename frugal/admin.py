7399d12f3282c431ea8ebb96da44da969cc3282b
admin <madhusona@gmail.com>
2018-11-27 16:31:23 +0530

    admin

--- frugal/admin.py
+++ frugal/admin.py
@@ -3,3 +3,5 @@ from .models import NGO,NGO_Profile,NGO_Registration
 
 # Register your models here.
 admin.site.register(NGO)
+admin.site.register(NGO_Profile)
+admin.site.register(NGO_Registration)
