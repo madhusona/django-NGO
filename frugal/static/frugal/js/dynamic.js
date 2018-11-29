4f225ca10b5a4bde1aa1d1e063cdf39525b6d100
admin <madhusona@gmail.com>
2018-11-29 14:45:21 +0530

    new

---
+++ frugal/static/frugal/js/dynamic.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+        
+    var max_fields = 3; //maximum input boxes allowed
+    var wrapper = $("form table:first"); //Fields wrapper
+    var add_button = $(".add_field_button"); //Add button ID
+    var remove_button = $(".remove_field_button");
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+      
+    e.preventDefault();
+    if(x < max_fields)
+    { //max input box allowed
+      x++; //text box increment
+      
+      $(wrapper).append('<tr id="row'+x+'"><td><input id="Recognized_Body'+x+'" class="form-control" name="Recognized_Body'+x+'" required="required" type="text" /></td>'+
+      ' <td><input id="Registration_Number'+x+'" class="form-control" name="Registration_Number'+x+'" required="required" type="text" /></td></tr>'); //add input box
+        
+    }
+    });
+
+    $(remove_button).click(function(e){ //on add input button click
+      
+      e.preventDefault();
+      if(x > 1)
+      { //max input box allowed
+        var row = $("tr#row"+x)
+        x--; //text box increment
+        row.remove()
+        
+      }
+      });
+
+      });
---
+++ frugal/static/frugal/js/geo.js
@@ -0,0 +1,78 @@
+function getLatitudeLongitude() {
+
+    var address = document.getElementById('id_City').value;
+    
+    //getLatitudeLongitude(address)
+    address = address || 'TamilNadu';
+   
+    // Initialize the Geocoder
+    var geocoder = new google.maps.Geocoder();
+    if (geocoder) {
+        
+        geocoder.geocode({
+            'address': address
+            }, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                document.getElementById('id_latitude').value = results[0].geometry.location.lat();
+                document.getElementById('id_longitude').value = results[0].geometry.location.lng();
+                loadMap();
+            }
+        });
+    }
+    
+}
+   
+
+function loadMap(){
+     
+    
+    var map = new GMap2(document.getElementById("map"));
+    map.addControl(new GLargeMapControl());
+    map.addControl(new GMapTypeControl());
+      
+                    
+    var lat = document.getElementById('id_latitude').value;
+    var lng = document.getElementById('id_longitude').value;
+    map.setCenter(new GLatLng(lat,lng), 12);
+   
+    GEvent.addListener(map, "click", function(overlay, point){
+        
+        map.clearOverlays();
+        if (point) {
+            map.addOverlay(new GMarker(point));
+            map.panTo(point);
+            document.getElementById("id_latitude").value = point.lat();
+            document.getElementById("id_longitude").value = point.lng();
+                        
+        }
+        var lat = parseFloat(document.getElementById("id_latitude").value);
+        var lng = parseFloat(document.getElementById("id_longitude").value);
+        var latlng = new google.maps.LatLng(lat, lng);
+        var geocoder = new google.maps.Geocoder();
+        
+        geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+    
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[1]) {
+                    msg = results[1].formatted_address
+                    document.getElementById("id_Address").value = msg
+                }
+            }
+        });
+    });
+
+
+   
+   
+
+}
+// arrange for our onload handler to 'listen' for onload events
+if (window.attachEvent) {
+window.attachEvent("onload", function() {
+getLatitudeLongitude();
+});
+} else {
+window.addEventListener("load", function() {
+getLatitudeLongitude();
+}, false);
+}
